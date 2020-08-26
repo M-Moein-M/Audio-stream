b9dae576c7f2c3925bc89b1f2b4930b31a4c5c34
M-Moein-M <mohammadmoein.moradi@gmail.com>
2020-08-26 12:42:10 +0430

    Playing the audio in client side instead of server side. Now the app has a button to load the data every time it gets clicked

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express');
-// const yas = require('youtube-audio-server');
-
-// hiding api key while sharing on github :P
-// require('dotenv').config();
-// const APP_KEY = process.env.APP_KEY;
-// yas.setKey(APP_KEY);
+const fs = require('fs');
+const stream = require('youtube-audio-stream')
 
 const app = express();
 app.use(express.static('public'));
@@ -15,15 +11,12 @@ app.listen(port, function () {
 });
 
 
-const stream = require('youtube-audio-stream')
-const url = 'http://youtube.com/watch?v=34aQNMvGEZQ'
-const decoder = require('lame').Decoder
-const Speaker = require('speaker')
-const speaker = new Speaker({
-    channels: 2,          // 2 channels
-    bitDepth: 16,         // 16-bit samples
-    sampleRate: 44100     // 44,100 Hz sample rate
+const url = 'https://www.youtube.com/watch?v=SsUjydIX2IA'
+
+app.get('/', function (req, res) {
+    return fs.createReadStream('public/index.html').pipe(res);
 });
 
-const data = stream(url).pipe(decoder());
-data.pipe(speaker);
+app.get('/audio', function (rq, res) {
+    stream(url).pipe(res);
+})
