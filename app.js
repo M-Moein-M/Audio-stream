231c02653a3ab59de650f7b96354e91e95f58e1e
M-Moein-M <mohammadmoein.moradi@gmail.com>
2020-08-26 11:30:12 +0430

    Playing a song using 'youtube-audio-stream' package

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const yas = require('youtube-audio-server');
+// const yas = require('youtube-audio-server');
 
 // hiding api key while sharing on github :P
-require('dotenv').config();
-const APP_KEY = process.env.APP_KEY.toString();
-yas.setKey(APP_KEY);
+// require('dotenv').config();
+// const APP_KEY = process.env.APP_KEY;
+// yas.setKey(APP_KEY);
 
 const app = express();
 app.use(express.static('public'));
@@ -15,14 +15,15 @@ app.listen(port, function () {
 });
 
 
-const id = 'HQmmM_qwG4k'; // "Whole Lotta Love" by Led Zeppelin.
-const file = 'whole-lotta-love.mp3';
-console.log(`Downloading ${id} into ${file}...`);
-yas.downloader
-    .onSuccess(({id, file}) => {
-        console.log(`Yay! Audio (${id}) downloaded successfully into "${file}"!`)
-    })
-    .onError(({id, file, error}) => {
-        console.error(`Sorry, an error ocurred when trying to download ${id}`, error)
-    })
-    .download({id, file})
+const stream = require('youtube-audio-stream')
+const url = 'http://youtube.com/watch?v=34aQNMvGEZQ'
+const decoder = require('lame').Decoder
+const Speaker = require('speaker')
+const speaker = new Speaker({
+    channels: 2,          // 2 channels
+    bitDepth: 16,         // 16-bit samples
+    sampleRate: 44100     // 44,100 Hz sample rate
+});
+
+const data = stream(url).pipe(decoder());
+data.pipe(speaker);
